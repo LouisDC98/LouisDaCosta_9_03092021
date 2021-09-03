748d9c561b2c98005959d777174dbb5b18eff9fa
LouisDC98 <louisdacosta98@gmail.com>
2021-09-03 18:10:10 +0200

    Improve minor css

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -6,24 +6,18 @@ import bike from '../../img/bike.svg';
 import bodybuilding from '../../img/bodybuilding.svg';
 
 function SideNav() {
+    const arrayImg = [yoga, swim, bike, bodybuilding];
     return (
         <React.Fragment>
             <div className="bgSideNav">
                 <nav className="navSideNav">
-                    <div className="linkSideNav">
-                        <img src={yoga} className="imgSideNav"></img>
-                    </div>
-                    <div className="linkSideNav">
-                        <img src={swim} className="imgSideNav"></img>
-                    </div>
-                    <div className="linkSideNav">
-                        <img src={bike} className="imgSideNav"></img>
-                    </div>
-                    <div className="linkSideNav">
-                        <img src={bodybuilding} className="imgSideNav"></img>
-                    </div>
+                    {arrayImg.map((data) => (
+                        <div className="linkSideNav" key={data}>
+                            <img src={data} className="imgSideNav"></img>
+                        </div>
+                    ))}
                 </nav>
-                <p className="textSideNav">Copyright, SprotSee 2020</p>
+                <p className="textSideNav">Copyright, SportSee 2020</p>
             </div>
         </React.Fragment>
     );
