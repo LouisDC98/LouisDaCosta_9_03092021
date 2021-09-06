a328e04416ab0f9a4f372dfa1d44472df3d08357
LouisDC98 <louisdacosta98@gmail.com>
2021-09-06 23:31:14 +0200

    Update graphs (WIP)

--- src/components/RadarGraph/RadarGraph.jsx
+++ src/components/RadarGraph/RadarGraph.jsx
@@ -1,16 +1,63 @@
-import React, { useEffect } from 'react';
-import './RadarGraph.css';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import {
+    Radar,
+    RadarChart,
+    PolarGrid,
+    PolarAngleAxis,
+    PolarRadiusAxis,
+    ResponsiveContainer
+} from 'recharts';
 import { getSessionIntensity } from '../../data/API';
 
 function RadarGraph(props) {
     const { selectedUser } = props;
+    const [intensity, setIntensity] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
-        getSessionIntensity(selectedUser.id).then((response) => {
-            console.log('Intensité session', response.data.data);
-        });
-    }, []);
-    return <div className="shapeRadarGraph">RadarChart</div>;
+        getSessionIntensity(selectedUser.id)
+            .then((response) => {
+                setIntensity(response.data.data);
+            })
+            .catch((error) => {
+                console.log(error);
+                setError(true);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, [selectedUser]);
+
+    if (loading) {
+        return <div>Loading</div>;
+    } else if (error) {
+        return <div>Erreur</div>;
+    } else {
+        return (
+            <RadarGraphShape>
+                <ResponsiveContainer width="100%" height="90%">
+                    <RadarChart cx={'center'} cy={'center'} outerRadius={100} data={intensity.data}>
+                        <PolarGrid />
+                        <PolarAngleAxis datakey="kind" />
+                        <PolarRadiusAxis tick={false} />
+                        <Radar dataKey="value" fill="red" fillOpacity={0.7} />
+                    </RadarChart>
+                </ResponsiveContainer>
+            </RadarGraphShape>
+        );
+    }
 }
+
+const RadarGraphShape = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #282d30;
+    width: 258px;
+    height: 263px;
+    border-radius: 5px;
+`;
 
 export default RadarGraph;
